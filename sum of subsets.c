64927063d6a649054a98a80b300f3cdfa16f74b9
veerannaB <107356128+veerannaB@users.noreply.github.com>
2022-06-22 21:41:01 +0530

    Update sum of subsets.c

--- sum of subsets.c
+++ sum of subsets.c
@@ -1,57 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-
-int set[50],sol[50],d;
-
-void SumOfSubsets(int s,int k,int r)
-{
-    sol[k]=1; //Select the element to Solution
-
-    if(s+set[k]==d)
-    {
-        printf("Selected Subset Elements\n");
-        for(int i=1;i<=k;i++)
-        {
-            if(sol[i])
-                printf("%d ",set[i]);
-        }
-        exit(0);
-    }
-    else if(s+set[k]+set[k+1]<=d)
-    {
-        SumOfSubsets(s+set[k],k+1,r-set[k]);
-    }
-
-    //backtracking
-    if((s+r-set[k]>=d) && (s+set[k+1]<=d))
-    {
-        sol[k]=0;
-        SumOfSubsets(s,k+1,r-set[k+1]);
-    }
-
-}
-
+int d;
+void sum(int,int,int[]);
 int main()
 {
-    int n,totalsum=0;
-    printf("Enter No.of Elements : ");
-    scanf("%d",&n);
-
-    printf("Enter the Elements : ");
-    for(int i=1;i<=n;i++)
-    {
-        scanf("%d",&set[i]);
-        totalsum+=set[i];
-    }
-
-    printf("Enter the Max Sum : ");
-    scanf("%d",&d);
-
-    if(totalsum<d || set[1]>d)
-    {
-        printf("No Solution\n");
-        exit(0);
-    }
-
-    SumOfSubsets(0,1,totalsum);
+    int n,w[100],i;
+    scanf("%d%d",&n,&d);
+    for(i=1;i<=n;i++)
+    scanf("%d",&w[i]);
+    sum(n,d,w);
 }
+
+void sum(int n,int d,int w[])
+{
+    int x[100],s,k,i;
+    for(i=1;i<=n;i++)
+    x[i]=0;
+    s=0;
+    k=1;
+    x[k]=1;
+    while(1)
+	{
+      if(k <= n && x[k]==1) {
+      if(s+w[k] == d) {
+	  for(i=1;i<=n;i++) {
+       if(x[i]==1)
+       printf("%d ",w[i]);
+	   }
+	    printf("\n");
+        x[k]=0;
+	}
+	 else if(s+w[k] < d)
+     s+=w[k];
+     else {
+        x[k]=0;
+		} }
+		 else{
+               k--;
+     while(k>0 && x[k]==0)
+     k--;
+     if(k<=0){
+         break;
+		 }
+       x[k]=0;
+       s=s-w[k];  }
+       k=k+1;
+       x[k]=1;
+ } }
